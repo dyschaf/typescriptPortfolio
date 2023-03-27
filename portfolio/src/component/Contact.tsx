f93ced461b13767ef0cb4de05142ac36ee00de32
dyschaf <schaffeldovid@gmail.com>
2023-03-27 18:51:48 +0300

    push add css and img

--- portfolio/src/component/Contact.tsx
+++ portfolio/src/component/Contact.tsx
@@ -1,22 +1,26 @@
+import Form from 'react-bootstrap/Form';
 function Contact (){
     return(
-        <>
+    <>
         <h1 id={"contact"}>Contact</h1>
         <hr />
         <p>Feel free to Contact me by submitting the form below and I will get back to you as soon as possible</p>
-        <form action="" method="get">
-            <label>Name:
-                <input name="name" type="text" />
-            </label>
-            <label>Email:
-                <input name="email" type="email" />
-            </label>
-            <label>Message:
-                <input name="message" type="text" />
-            </label>
-            <input type="submit"/>
-        </form>
-        </>
+
+        <Form className='form'>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Full Name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Example textarea</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+        </Form>
+    </>
     )
 }
 export default Contact
