e722de508dbd577f6561706fde004a53e7ee4048
dschaf <schaffeldovid@gmail.com>
2023-03-29 00:03:22 +0300

    mobile frendly

--- portfolio/src/component/Contact.tsx
+++ portfolio/src/component/Contact.tsx
@@ -5,7 +5,7 @@ function Contact (){
         <h1 className="center" id={"contact"}>Contact</h1>
         <hr />
         <p className="center">Feel free to Contact me by submitting the form below and I will get back to you as soon as possible</p>
-        <Form className='form'>
+        <Form action="https://formsubmit.co/schaffeldovid@gmail.com" method="POST">
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
             <Form.Label>Name</Form.Label>
             <Form.Control className="grey" type="text" placeholder="Full Name" />
