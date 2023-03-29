7825207b5307fd17e92c9c3d18099969f856f375
dyschaf <schaffeldovid@gmail.com>
2023-03-29 18:18:27 +0300

    ready for deploy

--- portfolio/src/component/Contact.tsx
+++ portfolio/src/component/Contact.tsx
@@ -9,15 +9,15 @@ function Contact (){
         <Form className='form'action="https://formsubmit.co/schaffeldovid@gmail.com" method="POST">
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
             <Form.Label>Name</Form.Label>
-            <Form.Control className="grey" type="text" name='name' placeholder="Full Name" />
+            <Form.Control className="grey" required type="text" name='name' placeholder="Full Name" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
             <Form.Label>Email address</Form.Label>
-            <Form.Control className="grey"type="email"name='email' placeholder="name@example.com" />
+            <Form.Control className="grey"type="email" required name='email' placeholder="name@example.com" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Label>Message</Form.Label>
-            <Form.Control  className="grey" name='message' as="textarea" rows={3} />
+            <Form.Control  className="grey" required name='message' as="textarea" rows={3} />
         </Form.Group>
         <Button variant="primary" type="submit">
         Submit
