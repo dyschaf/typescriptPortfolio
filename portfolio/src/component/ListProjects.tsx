502e79899d05aaddf38900c17165235a6ed59078
dyschaf <schaffeldovid@gmail.com>
2025-05-06 15:04:19 +0300

    fix spelling4

--- portfolio/src/component/ListProjects.tsx
+++ portfolio/src/component/ListProjects.tsx
@@ -15,7 +15,7 @@ export const projects = [
     ProjectPageMeetDescription:"This movie and TV show search engine fetches real-time data from an online database and saves search history in local storage, including last-watched episodes. It ensures a smooth experience with dynamic updates and will soon feature UI improvements, filters, a watchlist, and dark mode.",
     projectLink:"https://github.com/dyschaf/MovieDov",
     projectsDescription:"This movie and TV show search engine allows users to quickly find films and series by fetching real-time data from an online movie database API. It features local storage support, saving search history, last-watched episodes, and recent searches on the home screen for easy access. The app dynamically updates search results using the fetch API, ensuring a smooth, responsive experience. Future updates will enhance the UI design, introduce filtering options, a watchlist feature, dark mode, and mobile-friendly optimizations. While the core functionality is in place, the design is still a work in progress, with improvements planned to enhance usability and aesthetics.",
-    skillsUsed:["React","TypeScript","Bootsrtap","API","Local Storage","HTML","CSS","GIT","GitHub"],
+    skillsUsed:["React","TypeScript","Bootstrap","API","Local Storage","HTML","CSS","GIT","GitHub"],
     liveLink:"https://movie-dov.vercel.app"
   },
   {
@@ -27,7 +27,7 @@ export const projects = [
     ProjectPageMeetDescription:"This is my show case to show off my work to potenal companies of some of my prevous projects.",
     projectLink:"https://github.com/dyschaf/typescriptPortfolio",
     projectsDescription:"After considerable effort and dedication, I am thrilled to announce the successful completion of my updated portfolio. In an effort to enhance my skills and broaden my knowledge, I undertook the task of teaching myself Typescript for the very first time, and I am proud to say that it was a challenging yet rewarding experience. This newly updated portfolio, which I have written with the React framework, serves as a testament to my unwavering commitment to self-improvement and growth as a developer. It is a tangible representation of the work I have done and the progress I have made throughout my career thus far. As I showcase this portfolio to potential employers, I am confident that it will serve as a testament to my skills and my dedication to my craft. I am excited to demonstrate the depth of my knowledge and expertise, and to prove that I am a valuable asset to any team. Overall, this project has been an immensely fulfilling endeavor, and I am excited to see where my continued growth and development will take me in the future.",
-    skillsUsed:["React","TypeScript","Bootsrtap","HTML","CSS","GIT","GitHub"],
+    skillsUsed:["React","TypeScript","Bootstrap","HTML","CSS","GIT","GitHub"],
     liveLink:"https://typescript-portfolio-omega.vercel.app"
   },
   {
